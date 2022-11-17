bf8ca9456c678cb8a7d1bf9e50564ed201785431
feot <imboutodis@gmail.com>
2022-11-18 00:12:04 +0300

    fix codestyle

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
@@ -6,4 +6,4 @@ export default () => {
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
-}
+};
