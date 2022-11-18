59ff2f30fec4e4c93159d2c8235527569addd89b
feot <imboutodis@gmail.com>
2022-11-18 23:16:36 +0300

    add brain-calc game

---
+++ src/games/calc.js
@@ -0,0 +1,38 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from '../getRandomNumber.js';
+import gameBase from '../index.js';
+
+const gameRule = 'What is the result of the expression?';
+
+const gameData = () => {
+  const numberA = getRandomNumber(1, 100);
+  const numberB = getRandomNumber(1, 100);
+  const operatorIndex = getRandomNumber(1, 3);
+  let question;
+  let correctAnswer;
+
+  switch (operatorIndex) {
+    case 1:
+      correctAnswer = (numberA + numberB).toString();
+      question = `${numberA} + ${numberB}`;
+      break;
+    case 2:
+      correctAnswer = (numberA - numberB).toString();
+      question = `${numberA} - ${numberB}`;
+      break;
+    case 3:
+      correctAnswer = (numberA * numberB).toString();
+      question = `${numberA} * ${numberB}`;
+      break;
+    default:
+      break;
+  }
+
+  console.log(question);
+
+  const playerAnswer = readlineSync.question('Your answer: ');
+
+  return [correctAnswer, playerAnswer];
+};
+
+export default () => gameBase(gameRule, gameData);
