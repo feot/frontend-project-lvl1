6154972a2b7a43dbf508ae59bb3e3a04a436aa0e
feot <imboutodis@gmail.com>
2022-11-18 23:15:13 +0300

    refactor brain-even game

---
+++ src/games/even.js
@@ -0,0 +1,18 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from '../getRandomNumber.js';
+import gameBase from '../index.js';
+
+const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const gameData = () => {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = (number % 2 === 0) ? 'yes' : 'no';
+
+  console.log('Question: ', number);
+
+  const playerAnswer = readlineSync.question('Your answer: ');
+
+  return [correctAnswer, playerAnswer];
+};
+
+export default () => gameBase(gameRule, gameData);
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+export default (gameRule, game) => {
+  console.log('Welcome to the Brain Games!');
+
+  const playerName = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${playerName}!`);
+
+  let correctAnswersCount = 0;
+
+  console.log(gameRule);
+
+  while (correctAnswersCount < 3) {
+    const [correctAnswer, playerAnswer] = game();
+    const isAnswerCorrect = correctAnswer === playerAnswer;
+
+    if (isAnswerCorrect) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${playerName}!`);
+};
