311b9d88774af6cd280ef08d5f411aeabc96996f
feot <imboutodis@gmail.com>
2022-11-19 19:24:19 +0300

    add brain-progression game

---
+++ src/games/progression.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from '../getRandomNumber.js';
+import getProgression from '../getProgression.js';
+import gameBase from '../index.js';
+
+const gameRule = 'What number is missing in the progression?';
+
+const gameData = () => {
+  const progressionStart = getRandomNumber(1, 100);
+  const progressionLength = 10;
+  const progressionStep = getRandomNumber(1, 5);
+  const missingNumberIndex = getRandomNumber(0, progressionLength - 1);
+  const progression = getProgression(progressionStart, progressionLength, progressionStep);
+  const correctAnswer = progression[missingNumberIndex].toString();
+  const questionProgression = progression.map((item, i) => {
+    if (i === missingNumberIndex) {
+      return '..';
+    }
+
+    return item;
+  });
+
+  const question = (`Question: ${questionProgression.join(' ')}`);
+
+  console.log(question);
+
+  const playerAnswer = readlineSync.question('Your answer: ');
+
+  return [correctAnswer, playerAnswer];
+};
+
+export default () => gameBase(gameRule, gameData);
