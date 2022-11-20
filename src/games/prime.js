267639fb56a6d2eb7a1b8c7e87b4f6e26e32cf16
feot <imboutodis@gmail.com>
2022-11-20 19:52:09 +0300

    add brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,21 @@
+import readlineSync from 'readline-sync';
+import getRandomNumber from '../getRandomNumber.js';
+import getPrimeNumbers from '../getPrimeNumbers.js';
+import gameBase from '../index.js';
+
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const gameData = () => {
+  const number = getRandomNumber(1, 100);
+  const primeNumbers = getPrimeNumbers(100);
+  const question = (`Question: ${number}`);
+  const correctAnswer = (primeNumbers.includes(number)) ? 'yes' : 'no';
+
+  console.log(question);
+
+  const playerAnswer = readlineSync.question('Your answer: ');
+
+  return [correctAnswer, playerAnswer];
+};
+
+export default () => gameBase(gameRule, gameData);
