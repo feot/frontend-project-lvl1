03a640396e94b74c6b2dac3625a2af93c597ff81
feot <imboutodis@gmail.com>
2022-11-23 14:07:33 +0300

    refactor data generation

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,31 +3,37 @@ import gameBase from '../index.js';
 
 const gameRule = 'What is the result of the expression?';
 
+const getQuestionAndAnswer = (operatorIndex, numberA, numberB) => {
+  let question;
+  let correctAnswer;
+
+  switch (operatorIndex) {
+    case 1:
+      question = `${numberA} + ${numberB}`;
+      correctAnswer = (numberA + numberB).toString();
+      break;
+    case 2:
+      question = `${numberA} - ${numberB}`;
+      correctAnswer = (numberA - numberB).toString();
+      break;
+    case 3:
+      question = `${numberA} * ${numberB}`;
+      correctAnswer = (numberA * numberB).toString();
+      break;
+    default:
+      throw new Error('incorrect operatorIndex', operatorIndex);
+  }
+
+  return [question, correctAnswer];
+};
+
 const generateGameData = () => {
   const numberA = getRandomNumber();
   const numberB = getRandomNumber();
   const operatorIndexMin = 1;
   const operatorIndexMax = 3;
   const operatorIndex = getRandomNumber(operatorIndexMin, operatorIndexMax);
-  let question;
-  let correctAnswer;
-
-  switch (operatorIndex) {
-    case 1:
-      correctAnswer = (numberA + numberB).toString();
-      question = `${numberA} + ${numberB}`;
-      break;
-    case 2:
-      correctAnswer = (numberA - numberB).toString();
-      question = `${numberA} - ${numberB}`;
-      break;
-    case 3:
-      correctAnswer = (numberA * numberB).toString();
-      question = `${numberA} * ${numberB}`;
-      break;
-    default:
-      throw new Error('incorrect operatorIndex', operatorIndex);
-  }
+  const [question, correctAnswer] = getQuestionAndAnswer(operatorIndex, numberA, numberB);
 
   return [question, correctAnswer];
 };
