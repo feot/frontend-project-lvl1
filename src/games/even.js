0a99544c887ab74c4b5a1d250bf98a7d5c5d25a0
feot <imboutodis@gmail.com>
2022-11-24 12:12:10 +0300

    add exernal isEven func

--- src/games/even.js
+++ src/games/even.js
@@ -3,9 +3,11 @@ import gameBase from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => number % 2 === 0;
+
 const generateGameData = () => {
   const number = getRandomNumber();
-  const correctAnswer = (number % 2 === 0) ? 'yes' : 'no';
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
 
   return [number, correctAnswer];
 };
